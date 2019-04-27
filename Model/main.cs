1c954300db43f3c78777997e1949d3a351832443
Luis Paredes <luisparedez@gmail.com>
2019-04-26 20:10:55 -0400

    arregle el codigo de las letras que estaba confundido y lo hice mas complejo.

--- Model/main.cs
+++ Model/main.cs
@@ -11,7 +11,7 @@ namespace Practica1.Model
 
         public main()
         {
-            sr = new StreamReader("./Model/code.txt");
+            sr = new StreamReader("./Model/code.in");
         }
 
         public void Read()
@@ -25,9 +25,7 @@ namespace Practica1.Model
 
                 while (line != null)
                 {
-
-                   string a= workingLine(line);
-                    Console.WriteLine(a);
+                    Console.WriteLine(workingLine(line));
                     line = sr.ReadLine();
                 }
 
@@ -47,74 +45,27 @@ namespace Practica1.Model
 
         private string workingLine(string word)
         {
-            ArrayList ArrayLetter = new ArrayList();
             String Letter="";
             String number="";
-            char let=' ';
-            int numAct=0,numSg=0;
-
-            foreach (char letter in word)
-            {//Guardo letra por letra para trabajarlo mejor
-                ArrayLetter.Add(letter);
-            }
             
             for (int i = 0; i < word.Length; i++) {
                
-              if(IsNumeric(ArrayLetter[i])){//validar si es numero
+              if(IsNumeric(word[i])){//validar si es numero
                
-                  numAct = int.Parse(ArrayLetter[i].ToString());//el numero actual
-                 
-                  if(i+1 != word.Length  && IsNumeric(ArrayLetter[i+1].ToString()) ){
-                      
-                      numSg = int.Parse(ArrayLetter[i+1].ToString());
+               number += word[i];
 
-                  }else if(i+1 == word.Length){//si esta al final
+               if(convert(number)!='0'){
 
-                    number = number + numAct.ToString();
-                    let = convert(number);
-                    return Letter + let;
+                Letter += convert(number);
+                number="";
 
-                  }  
-                  
-                if(ArrayLetter[i].ToString().Equals("0")){
-                
-                if(!IsNumeric(ArrayLetter[i+1])){//si sigue una letra agrego el espacio
-                    Letter = Letter + " ";
-                    
-                }else{ //de lo contrario si no una letra evaluamos
-                    
-                    if(ArrayLetter[i+1].ToString().Equals("9") || ArrayLetter[i+1].ToString().Equals("1")){
-                         number = number + ArrayLetter[i].ToString();
-                    }else{
-                        Letter = Letter + " ";
-                    }
-                    
-                }
-                
-                }else if(!IsNumeric(ArrayLetter[i+1])){//si el numero que sigue es una letra
-
-                    number = number + numAct.ToString();
-                    let = convert(number);
-                    Letter = Letter + let;
+               }else if(number.Equals("0")){
+                   Letter += " ";
                     number="";
-
-                }else if(numAct<numSg){//si hay un numero mayor concatenar a numero
-                   
-                    number = number + ArrayLetter[i].ToString();
-                    
-                }else if(numAct>numSg){// si hay un numero menor convertir                   
-                    
-                    number = number + numAct.ToString();
-                    let = convert(number);
-                    number="";
-                    Letter = Letter + let;
-                     
-                }
+               }
                 
               }else{
-                 
-                  Letter = Letter + ArrayLetter[i].ToString();
-                  
+                  Letter += word[i];
               }   
                 
             }
@@ -138,7 +89,7 @@ namespace Practica1.Model
                 case "123457": return 'A';
                 case "1234567": return 'B';
                 case "456": return 'C';
-                case "0158": return 'D';
+                case "1580": return 'D';
                 case "12456": return 'E';
                 case "1249": return 'F';
                 case "12569": return 'G';
@@ -157,8 +108,8 @@ namespace Practica1.Model
                 case "278": return 'T';
                 case "13567": return 'U';
                 case "1379": return 'V';
-                case "0135679": return 'W';
-                case "09": return 'X';
+                case "1356790": return 'W';
+                case "90": return 'X';
                 case "1347": return 'Y';
                 case "23456": return 'Z';
 
